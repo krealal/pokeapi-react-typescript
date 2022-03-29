2bc52d65c679f7933553811dd2c006c25b5e1918
Kevin Real <kevinreal8994@gmail.com>
2022-03-29 18:53:41 +0200

    create main page skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import styled from "styled-components";
+
+const Main = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .imgTittle {
+    width: auto;
+    height: auto;
+  }
+  .menu {
+    display: flex;
+    width: 400px;
+    justify-content: space-around;
+    margin-top: 20px;
+  }
+  .menuOption {
+  }
+`;
+
 function App() {
-  return <p>hola</p>;
+  return (
+    <Main>
+      <img
+        src="./img/tittle.png"
+        alt="pokeapi tittle"
+        className="imgTittle"
+      ></img>
+      <div className="menu">
+        <a className="menuOption">Maing page</a>
+        <a className="menuOption">My team</a>
+        <a className="menuOption">Filter</a>
+      </div>
+    </Main>
+  );
 }
 
 export default App;
